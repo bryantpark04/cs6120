eff8da4b496e7220831163b7353a96456b1cf510
Bryant Park <bryantpark04@gmail.com>
2025-02-06 22:20:15 -0500

    Implement deleting assignments that are overwritten before being read

--- l3/tdce.ts
+++ l3/tdce.ts
@@ -1,5 +1,7 @@
 import { readStdin } from "../bril/bril-ts/util.ts";
 import { Function, Program, Instruction } from "../bril/bril-ts/bril.ts";
+import { formBasicBlocks } from "../l2/basic_blocks.ts";
+import { Block } from "../l2/types.ts";
 
 const main = async () => {
     const ast: Program = JSON.parse(await readStdin());
@@ -8,6 +10,7 @@ const main = async () => {
         let modified = true;
         while (modified) {
             modified = dceTrivial(func);
+            modified = deleteReassignBeforeRead(func) || modified;
         }
     });
     
@@ -19,12 +22,7 @@ const effectOps = new Set(["br", "jmp", "print", "ret", "call", "store", "free",
 const dceTrivial = (func: Function): boolean => {
     const originalLength = func.instrs.length;
 
-    const used: Set<string> = new Set();
-    func.instrs
-        .filter((inst): inst is Instruction & { args: string[] } => {
-            return "args" in inst && inst.args !== undefined;
-        })
-        .forEach(inst => inst.args.forEach(arg => used.add(arg)));
+    const used = new Set(func.instrs.flatMap(inst => "args" in inst ? inst.args : []));
 
     func.instrs = func.instrs
         .filter(inst => !(
@@ -33,6 +31,34 @@ const dceTrivial = (func: Function): boolean => {
         ));
 
     return originalLength != func.instrs.length;
+}
+
+const deleteReassignBeforeRead = (func: Function): boolean => {
+    const originalLength = func.instrs.length;
+    func.instrs = formBasicBlocks(func)
+        .flatMap(block => {
+            const used = new Set<string>(block.insts.flatMap(inst => "args" in inst && inst.args ? inst.args : []));
+            return block.insts
+                .toReversed()
+                .filter(inst => {
+                    if (!("dest" in inst)) {
+                        if ("args" in inst) {
+                            inst.args?.forEach(used.add.bind(used));
+                        }
+                        return true;
+                    }
+                    if (used.has(inst.dest)) {
+                        used.delete(inst.dest);
+                        if ("args" in inst) {
+                            inst.args?.forEach(used.add.bind(used));
+                        }
+                        return true;
+                    }
+                    return "op" in inst && effectOps.has(inst.op);
+                })
+                .toReversed();
+        });
+    return func.instrs.length != originalLength;
 }
 
 if (import.meta.main) main();
